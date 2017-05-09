4c6be387f4cc5993e030212118c532190d04c345
saadmk11 <saad.mk11@hotmail.com>
2017-05-10 03:31:28 +0600

    added buttons

--- pass_save/urls.py
+++ pass_save/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.details, name='details'),
-    url(r'^create/', views.create, name='add_new'),
+    url(r'^create/', views.create, name='create'),
     url(r'^(?P<id>\d+)/update/', views.update, name='update'),
     url(r'^(?P<id>\d+)/delete/', views.delete, name='delete'),
     url(r'^login/', views.login_view, name='login'),
